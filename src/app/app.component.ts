3a9ed955785a319cf10f814d66244a903a48f362
vasko7vas <vasko7vas@gmail.com>
2020-03-12 10:21:29 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,11 +29,6 @@ export class AppComponent  {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
   name: string = 'Storage';
-  sklad = [
-  {x:1, y:1, pall: 1, newfloor:0}, 
-  {x:1, y:2, pall: 0, newfloor:0}, 
-  {x:2, y:1,  pall: 2, newfloor:1},
-  {x:2, y:2,  pall: 1, newfloor:0}
-];
+  
 
 }
